7f163ef8ed7c827a61e487048404c0d4856c4d07
Supriya Sharma <sharmasup565@gmail.com>
2022-07-06 17:34:24 -0700

    finish routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,28 @@
 const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/userController.js');
 
 // /api/users
+router.route('/')
+    .get(getUsers)
+    .post(createUser);
 
 // /api/users/:userId
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/friendId')
+    .put(addFriend)
+    .delete(removeFriend);
 
 module.exports = router;
